8fe2a41126c30d9d8c9cc4ddeeed11ec6bc16a54
remonhasanapu <remon.hasan@technovista.com.bd>
2023-09-13 16:22:47 +0600

    connected mongoDB Database

--- Server.js
+++ Server.js
@@ -6,4 +6,9 @@ require('dotenv').config()
 const app = express()
 const PORT = process.env.port || 5000
 
+mongoose
+.connect(process.env.MONGOBD_URL)
+.then(() => console.log(`Connected to MONGODB!`))
+.catch((err) => console.log(err))
+
 app.listen( PORT, () => console.log(`Listening to the port ${PORT}`))
